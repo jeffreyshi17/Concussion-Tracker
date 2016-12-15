4c5c7d636290870e5c9ed89ee7535125120c0d0e
Jeffrey Shi <jeffrey.shi17@gmail.com>
2016-12-15 11:28:13 -0500

    concussion history update

--- public/CH/js/script.js
+++ public/CH/js/script.js
@@ -4,18 +4,18 @@ $(document).ready(function () {
     }
     $('input:checkbox').change(function () {
         if ($(this).is(":checked")) {
-            if (document.getElementById(this.id + "form")) {
-                document.getElementById(this.id + "form").style.display = "block";
-                var m = $("#" + this.id + "form").find('.suboption').find('input:checkbox')
+            if (document.getElementById(this.id + "_options")) {
+                document.getElementById(this.id + "_options").style.display = "block";
+                var m = $("#" + this.id + "_options").find('.suboption').find('input:checkbox')
                 for (var j = 0; j < m.length; j++) {
                     m[j].checked = false;
                 }
-                $("#" + this.id + "form").find('input:text').val('');
+                $("#" + this.id + "_options").find('input:text').val('');
             }
         }
         else {
-            if (document.getElementById((this).id + "form")) {
-                document.getElementById((this).id + "form").style.display = "none";
+            if (document.getElementById((this).id + "_options")) {
+                document.getElementById((this).id + "_options").style.display = "none";
             }
         }
     });
@@ -23,20 +23,21 @@ $(document).ready(function () {
         if ($(this).is(":checked")) {
             var x = document.getElementsByName(this.name);
             for (var i = 0; i < x.length; i++) {
-                if (document.getElementById(x[i].id + "form")) {
-                    document.getElementById(x[i].id + "form").style.display = "none";
-                    var m = $("#" + this.id + "form").find('.suboption').find('input:checkbox')
+                if (document.getElementById(x[i].id + "_options")) {
+                    document.getElementById(x[i].id + "_options").style.display = "none";
+                    var m = $("#" + this.id + "_options").find('.suboption').find('input:checkbox')
                     for (var j = 0; j < m.length; j++) {
                         m[j].checked = false;
                     }
-                    $("#" + x[i].id + "form").find('input:text').val('');
+                    $("#" + x[i].id + "_options").find('input:text').val('');
                 }
             }
-            if (document.getElementById(this.id + "form")) {
-                document.getElementById(this.id + "form").style.display = "block";
+            if (document.getElementById(this.id + "_options")) {
+                document.getElementById(this.id + "_options").style.display = "block";
             }
         }
     });
+    console.log("LOADED");
 });
 
 function checkFields(question) {
