75cd9f3542fdbfb1764a42bd7cdee9ae9d4489d0
jjanders <jjanders@users.noreply.github.com>
2017-01-18 10:57:49 -0500

    Update symptoms.js

--- public/SY/js/symptoms.js
+++ public/SY/js/symptoms.js
@@ -16,17 +16,30 @@ function storeToLocalStorage() {
     localStorage.setItem('Symptoms '+d, save);
 }
 function restoreFromLocalStorage() {
-	var max = localStorage.getItem(5);
+    var json;
+    for (var i = localStorage.length-1; i >= 0; i--){
+        if(localStorage.key(i).substring(0,8) == "Symptoms"){
+            json = JSON.parse(localStorage.getItem(localStorage.key(i)));
+            console.log(json);
+            break;
+        }
+    }
+    var input = document.querySelectorAll("input");
+	for(var i = 0; i < input.length; i++){
+        if(input[i].type == 'range'){
+            input[i].value = json.answers[i].value;
+        }
+		if(input[i].type == "text"){
+			input[i].value = json.answers[i].answer;
+		}
+	}
 }
-
 var clearLocal = function(){
 	localStorage.clear();
 }
-
 var createSummary = function(){
 window.location.href = "summary";
 }
-
 document.getElementById("save").addEventListener("click",storeToLocalStorage);
 document.getElementById("check").addEventListener("click",restoreFromLocalStorage);
 document.getElementById("clear").addEventListener("click",clearLocal);
