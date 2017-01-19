6ceb87c9bb90ce1ef618bf61f70aeda3e5fcfbf0
jjanders <jjanders@users.noreply.github.com>
2017-01-19 01:01:34 -0500

    Update results.js

--- public/SY/js/results.js
+++ public/SY/js/results.js
@@ -1,10 +1,10 @@
+var input = document.querySelectorAll("input");
 function restoreFromLocalStorage() {
     var json;
-
     for (var i = localStorage.length-1; i >= 0; i--){
         if(localStorage.key(i).substring(0,8) == "Symptoms"){
             json = JSON.parse(localStorage.getItem(localStorage.key(i)));
-			var input = document.querySelectorAll("input");
+
 			document.getElementById("date").innerHTML = localStorage.key(i).substring(8,localStorage.key(i).length);
 			for(var k = 0; k < input.length; k++){
 				if(input[k].type == 'range'){
@@ -15,20 +15,23 @@ function restoreFromLocalStorage() {
 				}
 				input[k].disabled = "true";
 			}
-			for(var k = input.length-1; k >= 0; k--){
-				console.log(input.length);
-				console.log(k+": "+input[k].value);
-				if(input[k].value == 0){
-					var table = document.getElementById('table');
-					var id = input[k].id;
-					var cut = parseInt(id.substring(3,id.length));
-					console.log(k+": "+cut);
-					table.deleteRow(cut+2);
-				}
-			}
+			cutRows();
             break;
         }
     }
+}
+function cutRows(){
+	for(var k = input.length-1; k >= 0; k--){
+		console.log(input.length);
+		console.log(k+": "+input[k].value);
+		if(input[k].value == 0){
+			var table = document.getElementById('table');
+			var id = input[k].id;
+			var cut = parseInt(id.substring(3,id.length));
+			console.log(k+": "+cut);
+			table.deleteRow(cut+2);
+		}
+	}
 }
 document.getElementById("container").addEventListener("load",restoreFromLocalStorage());
 
