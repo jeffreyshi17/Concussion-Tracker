775461680a249c59985a2418f212abee63571d2a
nicholasmullikin <nicholas.mullikin@gmail.com>
2017-01-19 11:42:05 -0500

    fixed

--- public/MT/js/scripts.js
+++ public/MT/js/scripts.js
@@ -3,7 +3,7 @@ $(function() {
 });
 
 
-function Submit(){
+function Submit (){
 
 var save = '{"title": "Menstrual Tracker",';
 
@@ -35,12 +35,16 @@ function display(){
 	var text = '<div class="container theme-showcase" role="main"> <div class="page-header"><h1>Menstrual Tracker Results</h1></div> <div class ="row"> <div class = "col-md-6"> <table class = "table" > <thead> <tr> <td>Date</td> <td>Last Period</td> <td>Menopausal</td> <td>Oral contraceptives</td> <td>Missed Periods</td> <td>Period Description</td> </tr> </thead> <tbody>';
 	for(var i = 0; i< localStorage.length; i++){
 		var json = JSON.parse(localStorage.getItem(localStorage.key(i)));
-		text+= '<tr>';
-		text+='<td>'+ json.date+'</td>';
-		for(var j = 0; j < json.answers.length; j++){
-			text +='<td>'+ json.answers[j].answer+'</td>';
+		if(typeof json.title != "undefined" && json.title == "Menstrual Tracker" ){
+			text+= '<tr>';
+			text+='<td>'+ json.date+'</td>';
+			
+				for(var j = 0; j < json.answers.length; j++){
+					text +='<td>'+ json.answers[j].answer+'</td>';
+				}
+			
+			text+= "</tr>";
 		}
-		text+= "</tr>";
 	}
 	text += '</tbody> </table> </div> </div> </div> </BODY> </HTML>';
 	document.body.innerHTML  = text;
