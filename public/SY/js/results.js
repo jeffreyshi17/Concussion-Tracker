f165c14464ba2018e1057bd9dea92de1098a641f
jjanders <jjanders@users.noreply.github.com>
2017-01-19 00:57:11 -0500

    Update results.js
    Cuts empty rows

--- public/SY/js/results.js
+++ public/SY/js/results.js
@@ -1,10 +1,9 @@
 function restoreFromLocalStorage() {
     var json;
-	var j;
+
     for (var i = localStorage.length-1; i >= 0; i--){
         if(localStorage.key(i).substring(0,8) == "Symptoms"){
             json = JSON.parse(localStorage.getItem(localStorage.key(i)));
-			j=i;
 			var input = document.querySelectorAll("input");
 			document.getElementById("date").innerHTML = localStorage.key(i).substring(8,localStorage.key(i).length);
 			for(var k = 0; k < input.length; k++){
@@ -17,15 +16,14 @@ function restoreFromLocalStorage() {
 				input[k].disabled = "true";
 			}
 			for(var k = input.length-1; k >= 0; k--){
-				//console.log(k);
-				//console.log(k+": "+input[k].value);
+				console.log(input.length);
+				console.log(k+": "+input[k].value);
 				if(input[k].value == 0){
 					var table = document.getElementById('table');
 					var id = input[k].id;
-					var cut = parseInt(id.substring(3,id.length-2)-1);
-					console.log(cut);
-					
-					table.deleteRow(cut);
+					var cut = parseInt(id.substring(3,id.length));
+					console.log(k+": "+cut);
+					table.deleteRow(cut+2);
 				}
 			}
             break;
