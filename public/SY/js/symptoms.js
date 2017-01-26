9cfed1ac88e8eda11a49e69203c8c305f1cfaf95
nicholasmullikin <nicholas.mullikin@gmail.com>
2017-01-26 11:22:54 -0500

    added some css viewport

--- public/SY/js/symptoms.js
+++ public/SY/js/symptoms.js
@@ -6,12 +6,13 @@ function storeToLocalStorage() {
     if(localStorage.length == 0){
         level = 0;
     }
+	level = 0;
     for (var i = localStorage.length-1; i >= 0; i--){
-        if(localStorage.key(i).substring(0,8) != "Symptoms"){
-            level = 0;
-        }
         if(localStorage.key(i).substring(0,8) == "Symptoms"){
-            json = JSON.parse(localStorage.getItem(localStorage.key(i)));
+			json = JSON.parse(localStorage.getItem(localStorage.key(i)));
+			if(isNaN(parseInt(json.level))){
+				level = 0;
+			}
             jtime = json.time;
             level = parseInt(json.level);
             break;
@@ -20,7 +21,7 @@ function storeToLocalStorage() {
     //RTP protocol
 
     var date = new Date();
-    var save = '{"title": "Symptom Tracker", "time": "'+date.getTime()+'", "level": "'+level.toString()+'", "answers": [';
+    var save = '{"title": "Symptom Tracker", "time": "'+date.getTime()+'", "level": "'+level+'", "answers": [';
     var body = document.querySelectorAll("input");
     for(var i = 0; i < body.length; i++){
         if(body[i].type == "range"){
@@ -49,10 +50,12 @@ function storeToLocalStorage() {
                     if(json.time - json2.time >= 86400000){
                         fullday = true;
                     }
-                    for(var x = 0; x <=40 ; x+=2){
-                        if(json2.answers[x].value < json.answers[x].value && level >= 1){
-                            change = true;
-                        }
+                    for(var x = 0; x < json2.answers.length; x+=2){
+						if(typeof json2.answers[x] != 'undefined'){
+							if(json2.answers[x].value < json.answers[x].value && level >= 1){
+								change = true;
+							}
+						}
                     }
                     break;
                 }
