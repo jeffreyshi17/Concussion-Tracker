ebde81846d91c22bff80880e306bf6030e2d8940
jjanders <jjanders@users.noreply.github.com>
2017-01-27 10:32:31 -0500

    Update symptoms.js

--- public/SY/js/symptoms.js
+++ public/SY/js/symptoms.js
@@ -1,41 +1,57 @@
 function storeToLocalStorage() {
-
+/*
     //RTP Protocol
     var level;
     var jtime;
     if(localStorage.length == 0){
         level = 0;
     }
-	level = 0;
     for (var i = localStorage.length-1; i >= 0; i--){
+        if(localStorage.key(i).substring(0,8) != "Symptoms"){
+            level = 0;
+        }
         if(localStorage.key(i).substring(0,8) == "Symptoms"){
-			json = JSON.parse(localStorage.getItem(localStorage.key(i)));
-			if(isNaN(parseInt(json.level))){
-				level = 0;
-			}
+            json = JSON.parse(localStorage.getItem(localStorage.key(i)));
             jtime = json.time;
             level = parseInt(json.level);
             break;
         }
     }
+    */
     //RTP protocol
 
     var date = new Date();
-    var save = '{"title": "Symptom Tracker", "time": "'+date.getTime()+'", "level": "'+level+'", "answers": [';
     var body = document.querySelectorAll("input");
-    for(var i = 0; i < body.length; i++){
-        if(body[i].type == "range"){
-            save +='{"id": "'+ body[i].id +'","value": "'+body[i].value+'"},';
-        }
+    if(localStorage.getItem('Symptoms') == null){
+        var save = '{"title": "Symptom Tracker", "form":[{"date": "'+date+'", "answers":[';
+
+        for(var i = 0; i < body.length; i++){
+            if(body[i].type == "range"){
+                save +='{"id": "'+ body[i].id +'","value": "'+body[i].value+'"},';
+            }
         /*if(body[i].type == "text"){
             save +='{"id": "'+ body[i].id +'","answer": "' + body[i].value +'"},';
         }*/
-    }
+        }
     save = save.substring(0, save.length - 1);
-    save += ']}';
-    var d = (date.getFullYear()+"/"+date.getMonth()+1+"/"+date.getDate());
-    localStorage.setItem('Symptoms '+d, save);
+    save += ']}]}';
+    localStorage.setItem('Symptoms',save);
+    }
+    else{
+        var save = localStorage.getItem('Symptoms');
+        save = save.substring(0, save.length-2);
+        save += ',{"date": "'+date+'", "answers":[';
+        for(var i = 0; i < body.length; i++){
+            if(body[i].type == "range"){
+                save +='{"id": "'+ body[i].id +'","value": "'+body[i].value+'"},';
+            }
+        }
+        save = save.substring(0, save.length-1);
+        save += ']}]}';
+        localStorage.setItem('Symptoms',save);
+    }
 
+    /*
     //RTP Protocol
     var change = false;
     var fullday = false;
@@ -50,12 +66,10 @@ function storeToLocalStorage() {
                     if(json.time - json2.time >= 86400000){
                         fullday = true;
                     }
-                    for(var x = 0; x < json2.answers.length; x+=2){
-						if(typeof json2.answers[x] != 'undefined'){
-							if(json2.answers[x].value < json.answers[x].value && level >= 1){
-								change = true;
-							}
-						}
+                    for(var x = 0; x <=40 ; x+=2){
+                        if(json2.answers[x].value < json.answers[x].value && level >= 1){
+                            change = true;
+                        }
                     }
                     break;
                 }
@@ -80,6 +94,7 @@ function storeToLocalStorage() {
     }
     localStorage.setItem("Symptoms "+d, JSON.stringify(json));
     //RTP Protocol
+    */
 }
 function restoreFromLocalStorage() {
     var json;
