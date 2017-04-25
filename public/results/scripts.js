2987597ca23674a46d010c9f6bd598fe7a125b99
Eli <elihschiff@gmail.com>
2017-04-25 11:18:30 -0400

    Add files via upload

--- public/results/scripts.js
+++ public/results/scripts.js
@@ -1,6 +1,6 @@
 function displayMT(){
 	var edits = false;
-	var text = '<div class="container theme-showcase" role="main"> <div class="page-header"><h1>Menstrual Tracker Results</h1></div> <div class ="row"> <div class = "col-md-6"> <table class = "table" > <thead> <tr> <td>Date</td> <td>Last Period</td> <td>Menopausal</td> <td>Oral contraceptives</td> <td>Missed Periods</td> <td>Period Description</td> </tr> </thead> <tbody>';
+	var text = '<div class="container theme-showcase" role="main"> <div class="page-header"><h1>Menstrual Tracker Results</h1></div> <div class ="row"> <div class = "col-md-6"> <table class = "table" > <thead> <tr> <td>Date</td> <td>Menopausal</td> <td>Last Period</td> <td>Oral contraceptives</td> <td>Missed Periods</td> <td>Period Description</td> </tr> </thead> <tbody>';
 	for(var i = 0; i< localStorage.length; i++){
 		var json = JSON.parse(localStorage.getItem(localStorage.key(i)));
 		if(typeof json.title != "undefined" && json.title == "Menstrual Tracker" ){
