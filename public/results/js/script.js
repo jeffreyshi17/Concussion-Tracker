098db5cad8d7d7cf9c35a51069f7dc54ee2ec397
Jeffrey Shi <jeffrey.shi17@gmail.com>
2017-05-05 02:06:10 -0400

    wow results page kinda works now!

--- public/results/js/script.js
+++ public/results/js/script.js
@@ -1,332 +1,154 @@
-function dashboard(id, fData) {
-    var barColor = 'steelblue';
-
-    function segColor(c) {
-        var colors = freqData[0].freq;
-
-        for (var property in colors) {
-            if (colors.hasOwnProperty(property)) {
-                property = "#" + Math.random().toString(16).slice(2, 8);
-            }
-        }
-        return colors[c];
-    }
-
-    // compute total for each state.
-    fData.forEach(function (d) {
-        d.total = 0;
-        for (var property in d.freq) {
-            if (d.freq.hasOwnProperty(property)) {
-                d.total += d.freq[property];
-            }
-        }
-    });
-
-    // function to handle histogram.
-    function histoGram(fD) {
-        var hG = {},
-            hGDim = {
-                t: 60,
-                r: 0,
-                b: 30,
-                l: 0
-            };
-        hGDim.w = 500 - hGDim.l - hGDim.r,
-            hGDim.h = 300 - hGDim.t - hGDim.b;
-
-        //create svg for histogram.
-        var hGsvg = d3.select(id).append("svg")
-            .attr("width", hGDim.w + hGDim.l + hGDim.r)
-            .attr("height", hGDim.h + hGDim.t + hGDim.b).append("g")
-            .attr("transform", "translate(" + hGDim.l + "," + hGDim.t + ")");
-
-        // create function for x-axis mapping.
-        var x = d3.scale.ordinal().rangeRoundBands([0, hGDim.w], 0.1)
-            .domain(fD.map(function (d) {
-                return d[0];
-            }));
-
-        // Add x-axis to the histogram svg.
-        hGsvg.append("g").attr("class", "x axis")
-            .attr("transform", "translate(0," + hGDim.h + ")")
-            .call(d3.svg.axis().scale(x).orient("bottom"));
-
-        // Create function for y-axis map.
-        var y = d3.scale.linear().range([hGDim.h, 0])
-            .domain([0, d3.max(fD, function (d) {
-                return d[1];
-            })]);
-
-        // Create bars for histogram to contain rectangles and freq labels.
-        var bars = hGsvg.selectAll(".bar").data(fD).enter()
-            .append("g").attr("class", "bar");
-
-        //create the rectangles.
-        bars.append("rect")
-            .attr("x", function (d) {
-                return x(d[0]);
-            })
-            .attr("y", function (d) {
-                return y(d[1]);
-            })
-            .attr("width", x.rangeBand())
-            .attr("height", function (d) {
-                return hGDim.h - y(d[1]);
-            })
-            .attr('fill', barColor)
-            .on("mouseover", mouseover) // mouseover is defined below.
-            .on("mouseout", mouseout); // mouseout is defined below.
-
-        //Create the frequency labels above the rectangles.
-        bars.append("text").text(function (d) {
-                return d3.format(",")(d[1])
-            })
-            .attr("x", function (d) {
-                return x(d[0]) + x.rangeBand() / 2;
-            })
-            .attr("y", function (d) {
-                return y(d[1]) - 5;
-            })
-            .attr("text-anchor", "middle");
-
-        function mouseover(d) { // utility function to be called on mouseover.
-            // filter for selected state.
-            var st = fData.filter(function (s) {
-                    return s.State == d[0];
-                })[0],
-                nD = d3.keys(st.freq).map(function (s) {
-                    return {
-                        type: s,
-                        freq: st.freq[s]
-                    };
-                });
-
-            // call update functions of pie-chart and legend.    
-            pC.update(nD);
-            leg.update(nD);
-        }
-
-        function mouseout(d) { // utility function to be called on mouseout.
-            // reset the pie-chart and legend.    
-            pC.update(tF);
-            leg.update(tF);
-        }
-
-        // create function to update the bars. This will be used by pie-chart.
-        hG.update = function (nD, color) {
-            // update the domain of the y-axis map to reflect change in frequencies.
-            y.domain([0, d3.max(nD, function (d) {
-                return d[1];
-            })]);
-
-            // Attach the new data to the bars.
-            var bars = hGsvg.selectAll(".bar").data(nD);
-
-            // transition the height and color of rectangles.
-            bars.select("rect").transition().duration(500)
-                .attr("y", function (d) {
-                    return y(d[1]);
-                })
-                .attr("height", function (d) {
-                    return hGDim.h - y(d[1]);
-                })
-                .attr("fill", color);
-
-            // transition the frequency labels location and change value.
-            bars.select("text").transition().duration(500)
-                .text(function (d) {
-                    return d3.format(",")(d[1])
-                })
-                .attr("y", function (d) {
-                    return y(d[1]) - 5;
-                });
-        }
-        return hG;
-    }
-
-    // function to handle pieChart.
-    function pieChart(pD) {
-        var pC = {},
-            pieDim = {
-                w: 250,
-                h: 250
-            };
-        pieDim.r = Math.min(pieDim.w, pieDim.h) / 2;
-
-        // create svg for pie chart.
-        var piesvg = d3.select(id).append("svg")
-            .attr("width", pieDim.w).attr("height", pieDim.h).append("g")
-            .attr("transform", "translate(" + pieDim.w / 2 + "," + pieDim.h / 2 + ")");
-
-        // create function to draw the arcs of the pie slices.
-        var arc = d3.svg.arc().outerRadius(pieDim.r - 10).innerRadius(0);
-
-        // create a function to compute the pie slice angles.
-        var pie = d3.layout.pie().sort(null).value(function (d) {
-            return d.freq;
-        });
-
-        // Draw the pie slices.
-        piesvg.selectAll("path").data(pie(pD)).enter().append("path").attr("d", arc)
-            .each(function (d) {
-                this._current = d;
-            })
-            .style("fill", function (d) {
-                return segColor(d.data.type);
-            })
-            .on("mouseover", mouseover).on("mouseout", mouseout);
-
-        // create function to update pie-chart. This will be used by histogram.
-        pC.update = function (nD) {
-            piesvg.selectAll("path").data(pie(nD)).transition().duration(500)
-                .attrTween("d", arcTween);
-        }
-        // Utility function to be called on mouseover a pie slice.
-        function mouseover(d) {
-            // call the update function of histogram with new data.
-            hG.update(fData.map(function (v) {
-                return [v.State, v.freq[d.data.type]];
-            }), segColor(d.data.type));
-        }
-        //Utility function to be called on mouseout a pie slice.
-        function mouseout(d) {
-            // call the update function of histogram with all data.
-            hG.update(fData.map(function (v) {
-                return [v.State, v.total];
-            }), barColor);
-        }
-        // Animating the pie-slice requiring a custom function which specifies
-        // how the intermediate paths should be drawn.
-        function arcTween(a) {
-            var i = d3.interpolate(this._current, a);
-            this._current = i(0);
-            return function (t) {
-                return arc(i(t));
-            };
-        }
-        return pC;
-    }
-
-    // function to handle legend.
-    function legend(lD) {
-        var leg = {};
-        // create table for legend.
-        var legend = d3.select(id).append("table").attr('class', 'legend');
-
-        // create one row per segment.
-        var tr = legend.append("tbody").selectAll("tr").data(lD).enter().append("tr");
-        // create the first column for each segment.
-        tr.append("td").append("svg").attr("width", '16').attr("height", '16').append("rect")
-            .attr("width", '16').attr("height", '16')
-            .attr("fill", function (d) {
-                return segColor(d.type);
-            });
-
-        // create the second column for each segment.
-        tr.append("td").text(function (d) {
-            return d.type;
-        });
-
-        // create the third column for each segment.
-        tr.append("td").attr("class", 'legendFreq')
-            .text(function (d) {
-                return d3.format(",")(d.freq);
-            });
-
-        // create the fourth column for each segment.
-        tr.append("td").attr("class", 'legendPerc')
-            .text(function (d) {
-                return getLegend(d, lD);
-            });
-
-        // Utility function to be used to update the legend.
-        leg.update = function (nD) {
-            // update the data attached to the row elements.
-            var l = legend.select("tbody").selectAll("tr").data(nD);
-
-            // update the frequencies.
-            l.select(".legendFreq").text(function (d) {
-                return d3.format(",")(d.freq);
-            });
-
-            // update the percentage column.
-            l.select(".legendPerc").text(function (d) {
-                return getLegend(d, nD);
-            });
-        }
-
-        function getLegend(d, aD) { // Utility function to compute percentage.
-            return d3.format("%")(d.freq / d3.sum(aD.map(function (v) {
-                return v.freq;
-            })));
-        }
-        return leg;
-    }
-
-    // calculate total frequency by segment for all state.
-    var tF = symptomNames.map(function (d) {
-        console.log(d);
-        return {
-            type: d,
-            freq: d3.sum(fData.map(function (t) {
-                return t.freq[d];
-            }))
-        };
-    });
-
-    // calculate total frequency by state for all segment.
-    var sF = fData.map(function (d) {
-        return [d.State, d.total];
-    });
-    console.log(tF);
-    var hG = histoGram(sF), // create the histogram.
-        pC = pieChart(tF), // create the pie-chart.
-        leg = legend(tF); // create the legend.
-}
-
 var freqData = [];
-var symptomNames = [];
-
+var symptomDictionary = {};
+var daily_data;
+if (localStorage.daily) {
+    daily_data = JSON.parse(localStorage.daily);
+}
+var daily_form;
+if (localStorage.daily) {
+    daily_form = JSON.parse(localStorage.dailyForm).form[0].questions;
+}
 $(document).ready(function () {
     loadData();
-    dashboard('#dashboard', freqData);
+    generateButtons();
+    generateDictionary();
+    updateData();
 });
 
-function loadData() {
-    var daily_data;
-    if (localStorage.daily) {
-        daily_data = JSON.parse(localStorage.daily);
+function generateDictionary() {
+    for (var k = 0; k < daily_form.length; k++) {
+        symptomDictionary[daily_form[k].answers[0].id] = daily_form[k].question;
     }
-    var daily_form;
-    if (localStorage.daily) {
-        daily_form = JSON.parse(localStorage.dailyForm).form[0].questions;
-    }
-
-    for (var i = 0; i < daily_data.length; i++) {
-        var dayData = {};
-        dayData.Date = daily_data[i].date;
-        var freq = {};
-        for (var j = 0; j < daily_data[i].answers[0].answers.length; j++) {
-            if (parseInt(daily_data[i].answers[0].answers[j].answer) === 0 || isNaN(daily_data[i].answers[0].answers[j].answer) || (daily_data[i].answers[0].answers[j].answer == "")) {
-
-            } else {
-                var id = daily_data[i].answers[0].answers[j].id;
-                var symptomName;
-                for (var k = 0; k < daily_form.length; k++) {
-                    if (id == daily_form[k].answers[0].id) {
-                        symptomName = daily_form[k].question;
-                        symptomNames.push(symptomName);
-                        break;
-                    }
-                }
-                freq[symptomName] = daily_data[i].answers[0].answers[j].answer;
-            }
-            dayData.freq = freq;
-            freqData.push(dayData);
-        }
-    }
-
-    console.log(freqData);
 }
 
-$(window).bind("load", function () {});
+function generateButtons() {
+    for (var i = 0; i < symptomNames.length; i++) {
+        var label = document.createElement('label');
+        label.className = 'btn btn-primary';
+        var button = document.createElement('input');
+        button.type = 'checkbox';
+        button.id = daily_form[i].answers[0].id;
+        button.autocomplete = 'off';
+        label.innerText = symptomNames[i];
+        label.appendChild(button);
+        $('#symptomButtons').append(label);
+    }
+    $('input:checkbox').change(function () {
+        updateData();
+    }); //for some reason, Headaches always saves as 0;
+}
+var dayData = {};
+var symptomNames = [];
+var symptomPoints = {};
+var dateLabels = [];
+
+function loadData() {
+    for (var i = 0; i < daily_data.length; i++) {
+        dateLabels.push(daily_data[i].date);
+        dayData[daily_data[i].date] = {};
+        dayData[daily_data[i].date].symptoms = [];
+        dayData[daily_data[i].date].points = [];
+        for (var j = 0; j < daily_data[i].answers[0].answers.length; j++) { //parse answers 
+            if (!(parseInt(daily_data[i].answers[0].answers[j].answer) === 0) && !(isNaN(daily_data[i].answers[0].answers[j].answer)) && !(daily_data[i].answers[0].answers[j].answer == "")) {
+                dayData[daily_data[i].date].symptoms.push(daily_data[i].answers[0].answers[j].id);
+                dayData[daily_data[i].date].points.push(daily_data[i].answers[0].answers[j].answer);
+            }
+        }
+    }
+    for (var k = 0; k < daily_form.length; k++) {
+        symptomNames.push(daily_form[k].question);
+    }
+    for (var k = 0; k < daily_form.length; k++) {
+        symptomPoints[daily_form[k].answers[0].id] = [];
+        console.log(daily_form[k].answers[0].id);
+        for (var date in dayData) {
+            var dataPoint = 0;
+            day = dayData[date];
+            if (day.symptoms.indexOf(daily_form[k].answers[0].id)!=-1) {
+                var pos = day.symptoms.indexOf(daily_form[k].answers[0].id);
+                dataPoint = day.points[pos];
+            }
+            symptomPoints[daily_form[k].answers[0].id].push(dataPoint);
+        }
+    }
+}
+var data;
+
+function updateData() {
+    data = {
+        labels: dateLabels
+        , datasets: [
+
+        ]
+        , options: {
+            legend: {
+                display: true
+                , labels: {
+                    fontColor: 'rgb(255, 99, 132)'
+                }
+            }
+        }
+    }
+    var inputs = document.getElementById('symptomButtons').getElementsByTagName('INPUT');
+    var i, e;
+    for (i = 0; i < inputs.length; ++i) {
+        e = inputs[i];
+        if (e.checked) {
+            var temp = {
+                label: symptomDictionary[e.id]
+                , fillColor: "rgba(220,220,220,0)"
+                , strokeColor: "rgba(220,220,220,1)"
+                , pointColor: "rgba(220,220,220,1)"
+                , pointStrokeColor: "#fff"
+                , data: symptomPoints[e.id]
+            }
+            data.datasets.push(temp);
+        }
+    }
+    var count = 10;
+    /*
+    var data = {
+            labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+            , datasets: [
+                {
+                    fillColor: "rgba(220,220,220,0)"
+                    , strokeColor: "rgba(220,220,220,1)"
+                    , pointColor: "rgba(220,220,220,1)"
+                    , pointStrokeColor: "#fff"
+                    , data: [65, 59, 90, 81, 56, 45, 30, 20, 3, 37]
+			}
+                , {
+                    fillColor: "rgba(151,187,205,0.5)"
+                    , strokeColor: "rgba(151,187,205,1)"
+                    , pointColor: "rgba(151,187,205,1)"
+                    , pointStrokeColor: "#fff"
+                    , data: [28, 48, 40, 19, 96, 87, 66, 97, 92, 85]
+			}
+		]
+        }
+        */
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myNewChart = new Chart(ctx);
+    myNewChart.Line(data, optionsNoAnimation);
+}
+
+function updateChart() {
+    myNewChart.Line(data, optionsNoAnimation);
+}
+var optionsAnimation = {
+        //Boolean - If we want to override with a hard coded scale
+        scaleOverride: true, //** Required if scaleOverride is true **
+        //Number - The number of steps in a hard coded scale
+        scaleSteps: 10, //Number - The value jump in the hard coded scale
+        scaleStepWidth: 1, //Number - The scale starting value
+        scaleStartValue: 0
+    }
+    // Not sure why the scaleOverride isn't working...
+var optionsNoAnimation = {
+    animation: false, //Boolean - If we want to override with a hard coded scale
+    scaleOverride: true, //** Required if scaleOverride is true **
+    //Number - The number of steps in a hard coded scale
+    scaleSteps: 10, //Number - The value jump in the hard coded scale
+    scaleStepWidth: 1, //Number - The scale starting value
+    scaleStartValue: 0
+}
